cf9ebd1d299d56a2dc3d2a8b5a36ba20d4898e99
ZawLinNaing <zawlinnaing52@gmail.com>
2026-09-09 06:56:57 +0630

    complete ex: 1.7. External access with Ingress

--- chapter-2 kubernetes-basics/exercise-1.1/log_output/main.go
+++ chapter-2 kubernetes-basics/exercise-1.1/log_output/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	// "crypto/rand"
+	// "encoding/hex"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
 )
+var randomString string
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	
+	currentTimestamp := time.Now().Format(time.RFC3339)
+	
+	// Format: <timestamp>: <randomString>
+	response := fmt.Sprintf("%s: %s\n", currentTimestamp, randomString)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(response))
+}
 
 func main (){
-	randomString := uuid.New().String()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	randomString = uuid.New().String()
 
 	// Function to print the timestamp and the stored string
 	logWithTimestamp := func() {
@@ -20,11 +44,20 @@ func main (){
 	logWithTimestamp()
 
 	// Create a ticker that ticks every 5 seconds
-	ticker := time.NewTicker(5 * time.Second)
-	defer ticker.Stop()
+	go func() {
+		ticker := time.NewTicker(5 * time.Second)
+		defer ticker.Stop()
 
-	// Loop indefinitely, waiting for the ticker
-	for range ticker.C {
-		logWithTimestamp()
+		for range ticker.C {
+			logWithTimestamp()
+		}
+	}()
+	
+	http.HandleFunc("/", statusHandler)
+
+	log.Printf("Server is listening on port %s...", port)
+
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
 	}
 }
