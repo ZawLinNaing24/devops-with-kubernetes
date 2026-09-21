3f731bfe98f9c26a39fb5568cc4a8bc66ef27aec
ZawLinNaing <zawlinnaing52@gmail.com>
2026-09-21 06:41:39 +0630

    complete exe 1.11. Persisting data

--- chapter-2 kubernetes-basics/exercise-1.1/log_output/reader-app/main.go
+++ chapter-2 kubernetes-basics/exercise-1.1/log_output/reader-app/main.go
@@ -5,23 +5,35 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"fmt"
+	"strings"
 )
+
+const pingpongFilePath = "/usr/src/app/files/pingpong.txt";
+const logFilePath = "/usr/src/app/files/log.txt";
 
 func statusHandler(w http.ResponseWriter, r *http.Request) {
 	// Writer ရေးနေတဲ့ Shared File ကို လှမ်းဖတ်မယ်
-	filePath := "/usr/src/app/files/log.txt"
-	data, err := os.ReadFile(filePath)
 	
+	// ၁။ Log File (Writer က ရေးထားသည်) ကို ဖတ်ပါ
+	logData, err := os.ReadFile(logFilePath)
 	if err != nil {
-		// ဖိုင် မရှိသေးရင် (သို့) ဖတ်လို့ မရသေးရင် ပြမယ့်စာ
+		// Log ဖိုင် မရှိသေးပါက Error ပြမည်
 		http.Error(w, "Log file not generated yet...", http.StatusNotFound)
 		return
 	}
+	// ၂။ Ping-pong Count ဖိုင်ကို ဖတ်
+	pingCount := "0" // Ping-pong ဖိုင် မရှိသေးပါက (သို့) မဖတ်နိုင်ပါက Default '0' ဟု သတ်မှတ်မည်
+	pingData, err := os.ReadFile(pingpongFilePath)
+	if err == nil {
+		pingCount = strings.TrimSpace(string(pingData))
+	}
 
+	responseText := fmt.Sprintf("%s.\nPing / Pongs: %s\n", strings.TrimSpace(string(logData)), pingCount)
 	// ဖတ်လို့ရတဲ့ စာကို Browser / curl ဆီ ပြန်ပို့မယ်
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
-	w.Write(data)
+	w.Write([]byte(responseText))
 }
 
 func main() {
--- chapter-2 kubernetes-basics/exercise-1.1/ping-pong/main.go
+++ chapter-2 kubernetes-basics/exercise-1.1/ping-pong/main.go
@@ -5,15 +5,40 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
+	"path/filepath"
 )
 
-var counter int = 0
+// ၁။ Package-level မှာ Variable တွေ ကြေညာခြင်း
+var (
+	counter int  = 0
+	dir string  = "/usr/src/app/files"
+	filePath string
+)
+
+// ၂။ init() function ထဲမှာ ဖိုလ်ဒါဆောက်ခြင်းနဲ့ Path သတ်မှတ်ခြင်း
+func init() {
+	os.MkdirAll(dir, os.ModePerm)
+	filePath = filepath.Join(dir, "pingpong.txt")
+}
 
 func pingpongHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile(filePath)
+	if err == nil {
+		// ဖိုင်ထဲမှ စာသားကို Integer ဂဏန်းသို့ ပြောင်းခြင်း
+		savedCount, convErr := strconv.Atoi(strings.TrimSpace(string(data)))
+		if convErr == nil {
+			counter = savedCount
+		}
+	}
 	// Request လာတိုင်း pong <counter> ဟု တုံ့ပြန်ပြီး counter ကို ၁ တိုးမည်
 	response := fmt.Sprintf("pong %d\n", counter)
 	counter++
-
+	err = os.WriteFile(filePath, []byte(strconv.Itoa(counter)), 0644)
+	if err != nil {
+		fmt.Printf("Error writing file: %v\n", err)
+	}
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(response))
